74d391da4fc75a7533f9290f38ac552a9cabbeb0
Вадим <Vadimk89@mail.ru>
2021-12-04 20:54:59 +0500

    Добавьте файлы проекта.

---
+++ 16Tsk/Program16.1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+using System.Text.Json.Serialization;
+
+namespace _16Tsk
+{
+    class Program
+    {
+        /*1 Разработать класс для моделирования объекта «Товар». 
+         * Предусмотреть члены класса «Код товара» (целое число), «Название товара» (строка), «Цена товара» (вещественное число).
+         * Создать массив из 5-ти товаров, значения должны вводиться пользователем с клавиатуры.
+         * Сериализовать массив в json-строку, сохранить ее программно в файл «Products.json». */
+        static void Main(string[] args)
+        {
+            //Задаем ввод адреса и названия файла, где будет храниться массив
+            Console.WriteLine("Введите адрес папки и через добавьте название файла с раширением");
+            string path = Console.ReadLine();
+            if (!File.Exists(path))
+            {
+                File.CreateText(path).Close();
+            }
+            //Задаем процедуру записи в файла по указанному адресу
+            using (StreamWriter sw = new StreamWriter(path, true))
+            {
+                //Задаем массив
+                Goods[] goods = new Goods[5];
+
+                for (int i = 0; i < goods.Length; i++)
+                {
+                    //Задаем ввод характеристик товара
+                    Goods goods1 = new Goods();
+                    Console.WriteLine("Введите код товара:");
+                    goods1.Code = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Введите название товара:");
+                    goods1.Name = Console.ReadLine();
+                    Console.WriteLine("Введите цену товара:");
+                    goods1.Price= Convert.ToDouble(Console.ReadLine());
+                    goods[i] = goods1;
+                }
+                //Задаем опции для JSON
+                JsonSerializerOptions option = new JsonSerializerOptions()
+                {
+                    Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic),
+                    WriteIndented = true
+                };
+                //Задаем строку JSON
+                string jsonstr = JsonSerializer.Serialize(goods, option);
+                //Записываем в файл
+                sw.Write(jsonstr);
+            }
+
+        }
+    }
+    //Задаем класс   
+    class Goods
+    {        
+        public int Code { get; set; }         
+        public string Name { get; set; }         
+        public double Price { get; set; }
+    }
+
+}
---
+++ 16tsk2/Program16.2.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+using System.Text.Json.Serialization;
+
+namespace _16tsk2
+{
+    /*Необходимо разработать программу для получения информации о товаре из json-файла.
+     * Десериализовать файл «Products.json» из задачи 1. Определить название самого дорогого товара.*/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задаем ввод адреса и названия файла, где будет храниться массив
+            Console.WriteLine("Введите адрес папки и через добавьте название файла с раширением");
+            string path = Console.ReadLine();
+            //Считываем файл и ищем наибольшую цену
+            JsonSerializerOptions option = new JsonSerializerOptions()
+            {
+                Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic),
+                WriteIndented = true
+            };
+            string jsonString = File.ReadAllText(path);
+            Goods[] goods = JsonSerializer.Deserialize<Goods[]>(jsonString, option);
+            double max = 0;
+            string most="";
+            foreach (var i in goods)
+            {
+                if (i.Price>=max)
+                {
+                    max = i.Price;
+                    most = i.Name;                        
+                }
+            }
+            Console.WriteLine("название самого дорогого товара {0}", most);        
+            Console.ReadKey();
+            }
+    }
+    class Goods
+    {        
+        public int Code { get; set; }        
+        public string Name { get; set; }        
+        public double Price { get; set; }
+
+    }
+
+}
